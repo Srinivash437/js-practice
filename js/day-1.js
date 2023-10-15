a73e51f651ebf0c86d7e9cb6e0e0dfe6a79aa5a4
Srinivash437 <nivassri832@gmail.com>
2023-10-15 12:45:13 +0530

    day-1-add-two-numbers

---
+++ js/day-1.js
@@ -0,0 +1,15 @@
+//declare values at const
+const num1 = 5;
+const num2 =  3;
+//add two values
+const sum = num1+num2
+//display the answer in console.log
+console.log("the sum of the num1 and num2 is:"+sum)
+
+//get values from prompt by user
+var a = parseInt(prompt("enter the first number"));
+var b = parseInt(prompt("enter the second number"));
+//add the two numbers
+var c = a + b;
+//display the answer in console.log
+console.log("the sum of a and b is:"+c);
