8c97cd0616a9c2393cd5ef830b20c658e7368b37
Srinivash437 <nivassri832@gmail.com>
2023-10-15 13:20:23 +0530

    day-2-square-root

---
+++ js/day-2.js
@@ -0,0 +1,18 @@
+//get input from user
+const num = prompt("Enter the number:");
+//using Math.sqrt to find squareroot
+const ans = Math.sqrt(num);
+//display the squareroot of the number in console.log
+console.log("the squareroot of the given number is:"+ans)
+ 
+//find the squareroot in static data
+const a = 10;
+//using Math.sqrt
+const result = Math.sqrt(a);
+//answer in console.log
+console.log("The squareroot of 10 is:"+result);
+
+//square root for negative values
+var x = -4;
+var ansx = Math.sqrt(x);
+console.log("The square root of -4 is:"+ansx);
