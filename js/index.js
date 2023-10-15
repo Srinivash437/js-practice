95db3dfa9f3e63ec3d3aa1fef62912e70c08cec5
Srinivash437 <nivassri832@gmail.com>
2023-10-15 12:26:26 +0530

    index-hello-world-program

---
+++ js/index.js
@@ -0,0 +1,6 @@
+//Print Hello World Program in alert
+alert ("'Hello World!'");
+//Print Hello World Program in console.log
+console.log ("Hello World!");
+//Print Hello World Program in Prompt
+prompt ("Hello World!")
